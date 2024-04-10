3a0942ea2f8109e6ed9b39623ac514a90e71a20c
Jeff Boek <jeff@jeffboek.com>
2024-04-10 09:18:14 -0700

    adds new case

---
+++ src/app/1889066/button.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Button() {
+    return (<button
+      className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+      type="submit"
+      onClick={(_) => window.open("https://samesitestrict.vercel.app/1889066")}>
+        Stage 2
+    </button>)
+  }
+  
